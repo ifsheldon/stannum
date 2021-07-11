0ca7793079a2c6384cd45399a95d3b1080db7eb2
feng liang <feng.liang@kaust.edu.sa>
2021-07-11 16:45:32 +0300

    finished kernel chain implementation, not tested yet, wait for Taichi upstream fix of a bug
    bug: no __name__ in a @ti.kernel method in a @ti.data_oriented class

--- stannum/stannum.py
+++ stannum/stannum.py
@@ -10,18 +10,29 @@ class FieldType(Enum):
 
 class TinConfigs:
     def __init__(self,
-                 ti_kernel,
+                 ti_kernel_bundles,
                  input_fields,
                  weight_fields,
                  output_fields,
-                 device,
-                 *kernel_args):
-        self.ti_kernel = ti_kernel
+                 device):
+        self.kernel_bundles = ti_kernel_bundles
         self.input_fields: [TaichiField] = input_fields
         self.weight_fields: [TaichiField] = weight_fields
         self.output_fields: [TaichiField] = output_fields
-        self.kernel_args = kernel_args
         self.device: torch.device = device
+
+
+class TaichiKernelBundle:
+    def __init__(self, kernel, *args):
+        self.kernel = kernel
+        self.name = kernel.__name__
+        self.args = args
+
+    def forward(self):
+        self.kernel(*self.args)
+
+    def backward(self):
+        self.kernel.grad(*self.args)
 
 
 class TaichiField:
@@ -49,7 +60,8 @@ class TinFunc(torch.autograd.Function):
         assert len(input_tensors) == len(all_input_fields)
         for input_tensor, field in zip(input_tensors, all_input_fields):
             field.from_torch(input_tensor)
-        tin_configs.ti_kernel(*tin_configs.kernel_args)
+        for kernel_bundle in tin_configs.kernel_bundles:
+            kernel_bundle.forward()
         output_tensors = []
         for output_field in tin_configs.output_fields:
             output_tensor = output_field.to_torch(device=tin_configs.device).requires_grad_(True)
@@ -66,7 +78,8 @@ class TinFunc(torch.autograd.Function):
         for grad_output, output_field in zip(grad_outputs, tin_configs.output_fields):
             if output_field.needs_grad:
                 output_field.grad.from_torch(grad_output)
-        tin_configs.ti_kernel.grad(*tin_configs.kernel_args)
+        for kernel_bundle in reversed(tin_configs.kernel_bundles):
+            kernel_bundle.backward()
         gradient_tensors = [None]
         for input_field in tin_configs.input_fields:
             if input_field.needs_grad:
@@ -90,7 +103,8 @@ class Tin(torch.nn.Module):
         self.device = device
         self.tin_func = TinFunc()
         self.tin_configs = None
-        self.kernel = None
+        self.kernel_bundles = []
+        self.kernel_bundle_dict = {}
         self.kernel_args = None
         self.finished = False
 
@@ -112,12 +126,15 @@ class Tin(torch.nn.Module):
         self.weight_fields[field_name] = TaichiField(field, FieldType.WEIGHTS, needs_grad)
         return self
 
-    def register_kernel(self, kernel):
+    def register_kernel(self, kernel, *kernel_args):
+        assert not self.finished
         assert kernel is not None
         if isinstance(kernel, str):
-            self.kernel = getattr(self.data_oriented, kernel)
-        else:
-            self.kernel = kernel
+            kernel = getattr(self.data_oriented, kernel)
+        kernel_bundle = TaichiKernelBundle(kernel, kernel_args)
+        assert kernel_bundle.name not in self.kernel_bundle_dict
+        self.kernel_bundles.append(kernel_bundle)
+        self.kernel_bundle_dict[kernel_bundle.name] = kernel_bundle
         return self
 
     def set_weight_field(self, field_name, tensor):
@@ -127,21 +144,23 @@ class Tin(torch.nn.Module):
         assert field_name in self.weight_fields
         self.weight_fields[field_name].from_torch(tensor)
 
-    def set_kernel_args(self, *kernel_args):
-        self.kernel_args = kernel_args
-        if self.finished:
-            self.tin_configs.kernel_args = kernel_args
+    def set_kernel_args(self, kernel, *kernel_args):
+        if isinstance(kernel, str):
+            kernel_name = kernel
+        else:
+            kernel_name = kernel.__name__
+        assert kernel_name in self.kernel_bundle_dict
+        self.kernel_bundle_dict[kernel_name].args = kernel_args
 
     def finish(self):
         assert len(self.input_fields) > 0
         assert len(self.output_fields) > 0
-        assert self.kernel is not None
-        self.tin_configs = TinConfigs(self.kernel,
+        assert len(self.kernel_bundles) > 0
+        self.tin_configs = TinConfigs(self.kernel_bundles,
                                       self.input_fields,
                                       list(self.weight_fields.values()),
                                       self.output_fields,
-                                      self.device,
-                                      self.kernel_args)
+                                      self.device)
         self.finished = True
         return self
 
