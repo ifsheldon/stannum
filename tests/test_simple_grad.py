837d771361a5bd9c7ea2039168235d2789410502
feng liang <feng.liang@kaust.edu.sa>
2021-07-13 23:57:20 +0300

    added a test func

--- tests/test_simple_grad.py
+++ tests/test_simple_grad.py
@@ -18,7 +18,7 @@ class Multiplier:
             self.output_field[i] = self.multiplier[None] * self.input_field[i] + num
 
 
-if __name__ == "__main__":
+def test_grad_with_data_oriented_class():
     ti.init(ti.cpu, default_fp=ti.f32)
     data_oriented = Multiplier(2.0)
     device = torch.device("cpu")
@@ -30,23 +30,18 @@ if __name__ == "__main__":
         .finish()
     tin_layer.set_kernel_args(1.0)
     data_tensor = torch.tensor([0.5, 0.5]).requires_grad_(True).to(device)
-    print(f"data = {data_tensor}")
     w1 = torch.ones(2).requires_grad_(True).to(device)
     w2 = torch.tensor([2.0, 2.0]).requires_grad_(True).to(device)
     output1 = w1 * data_tensor
-    print(f"output1 = {output1}")
     output2 = tin_layer(output1)
-    output2.retain_grad()
-    print(f"output2 = {output2}")
     output3 = output2 * w2
-    print(f"output3 = {output3}")
     loss = output3.sum()
     loss.backward()
-    print(f"w1 = {w1}\nw2 = {w2}")
-    print(f"gradients:\n"
-          f"w1 grad = {w1.grad}\n"
-          f"w2 grad = {w2.grad}\n"
-          f"output2 grad = {output2.grad}\n"
-          f"data tensor grad = {data_tensor.grad}\n"
-          f"multiplier grad = {data_oriented.multiplier.grad}"
-          )
+    assert torch.allclose(w1.grad, torch.tensor(2.0))
+    assert torch.allclose(w2.grad, torch.tensor(2.0))
+    assert torch.allclose(data_tensor.grad, torch.tensor(4.0))
+    assert torch.allclose(data_oriented.multiplier.grad.to_torch(device), torch.tensor(2.0))
+
+
+if __name__ == "__main__":
+    test_grad_with_data_oriented_class()
