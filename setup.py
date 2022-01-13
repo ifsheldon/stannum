fc6f67d3db2be2ee226c0752c6504be2b2eda6a1
Feng Liang <feng.liang@kaust.edu.sa>
2022-01-14 00:00:50 +0300

    bump alpha version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stannum",
-    version="0.4.0-alpha.2",
+    version="0.4.0-alpha.3",
     author="Feng Liang",
     author_email="feng.liang@kaust.edu.sa",
     description="PyTorch wrapper for Taichi data-oriented class",
