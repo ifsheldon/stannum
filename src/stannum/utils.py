3fa0ab842b5aa04909331b33187a5200f99e5ec0
Feng Liang <feng.liang@kaust.edu.sa>
2022-02-23 22:15:15 +0300

    fix needs_grad not found problem on new Taichi

--- src/stannum/utils.py
+++ src/stannum/utils.py
@@ -61,7 +61,7 @@ def check_field_needs_grad(field: Union[MatrixField, ScalarField], needs_grad: U
                 f"You are using legacy Taichi (v{__ti_version[0]}.{__ti_version[1]}.{__ti_version[2]} < v0.7.26), "
                 f"you need to specify needs_grad yourself when registering a field")
         else:
-            return field.snode.needs_grad
+            return field.snode.ptr.has_grad()
     return needs_grad
 
 
