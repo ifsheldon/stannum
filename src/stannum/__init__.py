85b623b5b76d334d831540841ad0c14ff6f127b3
Feng Liang <feng.liang@kaust.edu.sa>
2022-12-29 22:51:35 +0800

    add export

--- src/stannum/__init__.py
+++ src/stannum/__init__.py
@@ -1,5 +1,8 @@
 from .tin import Tin, EmptyTin
 from .tube import Tube
-from .auxiliary import MatchDim, AnyDim, BatchDim, DimOption, DimID
+from .auxiliary import MatchDim, AnyDim, BatchDim, DimOption, DimID, FieldManager, DimensionCalculator
 
-__all__ = ["Tin", "EmptyTin", "Tube", "MatchDim", "AnyDim", "BatchDim", "DimOption", "DimID"]
+__all__ = ["Tin", "EmptyTin",
+           "Tube",
+           "MatchDim", "AnyDim", "BatchDim", "DimOption", "DimID",
+           "FieldManager", "DimensionCalculator"]
